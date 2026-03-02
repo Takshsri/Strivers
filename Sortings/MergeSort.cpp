bce03a0dc257c72282813668d86d04a3ba1abdc9
Takshsri <aymar131221@gmail.com>
2026-03-02 10:40:26 +0530

    Adding Quick and Merge Sort

---
+++ Sortings/MergeSort.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int>& a,int low,int high,int mid){
+    vector<int> temp;
+    int left = low;
+    int right = mid+1;
+    while(left <= mid && right <= high){
+        if(a[left]<=a[right]){
+            temp.push_back(a[left++]);
+            
+        }
+        else{
+            temp.push_back(a[right++]);
+           
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(a[left++]);
+        
+    }
+    while(right <= high){
+        temp.push_back(a[right++]);
+       
+    }
+    for(int i=0;i<temp.size();i++){
+        a[low+i] = temp[i];
+    }
+}
+void  mergeSort(vector<int>& a,int l,int r){
+    if(l>=r) return;
+
+    int mid = l +(r-l)/2;
+    mergeSort(a,l,mid);
+    mergeSort(a,mid+1,r);
+
+    merge(a,l,r,mid);
+
+}
+int main(){
+    vector<int> a = {45,2,34,1,67,7,5,4};
+    mergeSort(a,0,7);
+    for(int i : a){
+        cout<<i<<" ";
+    }
+}
---
+++ Sortings/QuickSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Pivot(vector<int>& a,int l ,int r){
+    int pivot = a[l];
+    int i = l;
+    int j = r;
+
+    while(i < j){
+
+        while(i <= r && a[i] <= pivot) i++;
+        while(j >= l && a[j] > pivot) j--;
+
+        if(i < j) swap(a[i], a[j]);
+    }
+
+    swap(a[l], a[j]);
+    return j;
+}
+
+void Quicksort(vector<int>& a,int l,int r){
+    if(l < r){
+        int p = Pivot(a, l, r);
+
+        Quicksort(a, l, p-1);
+        Quicksort(a, p+1, r);
+    }
+}
+
+int main(){
+    vector<int> a = {23,56,3,1,67,33,12};
+
+    Quicksort(a,0,a.size()-1);
+
+    for(int x : a)
+        cout<<x<<" ";
+}

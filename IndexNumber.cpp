4f9cbd6667bcfc53a369bafd55a23f7ef26177ca
Takshsri <aymar131221@gmail.com>
2026-03-29 11:33:24 +0530

    Adding files on Index Number and Majority Element

--- IndexNumber.cpp
+++ IndexNumber.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+//O(n) time complexity and O(1) space complexity
 int Index(vector<int>& a){
     int n = a.size();
     for(int i=0;i<n;i++){
@@ -11,10 +12,29 @@ int Index(vector<int>& a){
     return -1;
 }
 
+//O(log n)  time complexity and O(1) space complexity
+int IndexBinary(vector<int>& a){
+    int l=0;
+    int r = a.size()-1;
+    while(l<=r){
+        int mid = l + (r-l)/2;
+        if(a[mid]==mid){
+            return mid;
+        }
+        else if(a[mid]<mid){
+            l= mid+1;
+        }
+        else{
+            r = mid-1;
+        }
+    }
+    return -1;
+}
+
 int main(){
-     vector<int> arr = {-6,-4,1,5,7};
-     if(Index(arr)!=-1){
-        cout<<"Index Number is :"<<Index(arr)<<endl;
+     vector<int> arr = {-6,-4,2,2,5,7};
+     if(IndexBinary(arr)!=-1){
+        cout<<"Index Number is :"<<IndexBinary(arr)<<endl;
      }
      else{
         cout<<"No Index Number Found"<<endl;

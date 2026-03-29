c7a7a8b73e82a076c0794b74c330df0edd097378
Takshsri <aymar131221@gmail.com>
2026-03-29 11:37:47 +0530

    Adding full code of Majority Element

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -2,7 +2,23 @@
 #include<vector>
 
 using namespace std;
-
+int MajorityEle(vector<int>& a){
+    int ans = 0;
+    int freq =0;
+    for(int i=0;i<a.size();i++){
+        if(freq==0){
+            ans = a[i];
+        }
+        if(ans == a[i]){
+            freq++;
+        }
+        else{
+            freq--;
+        }
+    }
+    return ans;
+}
 int main(){
-    vector<int> arr = {}
+    vector<int> arr = {1,2,3,3};
+    cout<<"Majority Element :"<<MajorityEle(arr);
 }

c800a904d479c973dea7d0e4b86d0ac614067abf
Takshsri <aymar131221@gmail.com>
2026-05-15 21:56:28 +0530

    adding leetcode 1299 question

---
+++ Leetcode1299.cpp
@@ -0,0 +1,30 @@
+//Replace 1299. Replace Elements with Greatest Element on Right Side
+
+// Given an array arr, 
+//replace every element in that array with the greatest element among the elements to its right, 
+//and replace the last element with -1.
+
+// After doing so, return the array.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> Greater(vector<int>& a){
+    int n = a.size();
+    int maxRight =  -1;
+    for(int i=n-1;i>=0;i--){
+        int current = a[i];
+        a[i] = maxRight;
+        maxRight = max(maxRight,current);
+
+    }
+    return a ;
+}
+int main(){
+    vector<int> a = {17,18,5,4,6,1};
+    vector<int> res = Greater(a);
+    for(int i : res){
+        cout<<i<<" ";
+    }
+}
